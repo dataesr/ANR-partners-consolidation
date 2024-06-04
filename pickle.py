ccd6a5cfac210ec0fb1a1637ca21a6a752f5590d
hfsllt <hafsa.lt21@gmail.com>
2024-06-04 10:46:30 +0200

    save code

---
+++ pickle.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import pickle
+
+def write_cache(cached_openalex_data,path):
+    pickle.dump(cached_openalex_data, open(path, 'wb'))
+
+def load_cache(cached_openalex_data,path):
+    cached_openalex_data = pickle.load(open(path, 'rb'))
+    print(f'{len(cached_openalex_data)} data in cache')
+    return cached_openalex_data
